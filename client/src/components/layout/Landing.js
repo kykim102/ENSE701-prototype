7721552c7ed319419aedb62ef6d96c265e953391
Kyu <kykim102@gmail.com>
2020-09-30 13:56:29 +1300

    Search bar on main page

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
+  const [formData, setFormData] = useState({
+    title: "",
+  })
+
+  const { title } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("SUCCESS");
+  };
+
   return (
     <section className='landing'>
       <div className='dark-overlay'>
         <div className='landing-inner'>
+
           <h1 className='x-large'>Hello SEER!</h1>
           <p className='lead'>Prototype by 2020 ENSE701 Sem2 Group 3</p>
           <div className='buttons'>
@@ -15,6 +30,20 @@ const Landing = () => {
             <Link to='/login' className='btn btn-light'>
               Login
             </Link>
+
+          <p className='searchHead'> Search </p>
+          <form className='searchForm' onSubmit={(e) => onSubmit(e)}>
+            <div className='form-group'>
+              <input 
+              type='title'
+              placeholder='Enter title here'
+              name='title'
+              value={title}
+              onChange={(e) => onChange(e)}
+              />
+              <input type='submit' className='btn btn-primary' value='Search' />
+            </div>
+          </form>
           </div>
         </div>
       </div>
